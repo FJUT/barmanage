a3d77485302f39cbe13bc0e3cb69417a23d15cbc
WHUP\99171 <nan.shi@upchina.com>
2017-03-21 10:03:07 +0800

    假数据

--- test/fake.js
+++ test/fake.js
@@ -4,57 +4,66 @@
  */
 var co = require('co')
 var faker = require('faker/locale/zh_CN')
-var { User, Message, Bar } = require('../models')
+var models = require('../models')
+var { User, Message, Bar } = models
 
- //var users = []
- //for (var i = 0; i < 100; i++) {
- //  users.push({
- //    name: faker.name.findName(),
- //    avatar: faker.image.avatar(),
- //    openid: faker.random.uuid(),
- //    level: faker.random.number(),
- //    score: faker.random.number(),
- //    password: ''
- //  })
- //}
- //
- //User.bulkCreate(users)
- //var messages = []
- //
- //for (var i = 0; i < 2; i++) {
- //  messages.push({
- //    message: i % 2 > 0 ? faker.image.image() : faker.lorem.sentence(),
- //    photo: i % 2 > 0 ? 1: 0,
- //    time: faker.date.past(),
- //    BarId: parseInt(Math.random() * 20),
- //    UserId: parseInt(Math.random() * 100)
- //  })
- //}
-//
-// Message.bulkCreate(messages)
+const random = (min, max) => {
+  return Math.floor(Math.random() * max) + min
+}
 
-var bars = []
+async function createBars() {
+  var bars = []
 
-//for (var i = 0; i < 10; i++) {
-//  bars.push({
-//    logo: faker.image.avatar(),
-//    name: faker.name.firstName(),
-//    address: faker.name.lastName(),
-//    summary: faker.lorem.sentence()
-//  })
-//}
-//
-//Bar.bulkCreate(bars)
+  for (var i = 0; i < 10; i++) {
+    bars.push({
+      logo: faker.image.avatar(),
+      name: faker.name.firstName(),
+      address: faker.name.lastName(),
+      summary: faker.lorem.sentence()
+    })
+  }
 
-co(function*() {
-  var user = yield User.findById(1)
-  var bar = yield Bar.findById(1)
+  return Bar.bulkCreate(bars)
+}
 
-  Message.create({
-    message: faker.lorem.sentence(),
-    photo: 0,
-    time: faker.date.past(),
-    BarId: bar.get('id'),
-    UserId: user.get('id')
-  })
-})
+async function createUsers() {
+  var users = []
+
+  for (var i = 0; i < 100; i++) {
+   users.push({
+     name: faker.name.findName(),
+     avatar: faker.image.avatar(),
+     openid: faker.random.uuid(),
+     level: faker.random.number(),
+     score: faker.random.number(),
+     password: ''
+   })
+  }
+
+  return User.bulkCreate(users)
+}
+
+async function createMessages() {
+  for (let i = 0; i < 200; i++) {
+    let user = await User.findById(random(1, 100))
+    let bar = await User.findById(random(1, 10))
+
+    await Message.create({
+      message: i % 2 > 0 ? faker.image.image() : faker.lorem.sentence(),
+      photo: i % 2 > 0 ? 1: 0,
+      time: faker.date.past(),
+      BarId: bar.get('id'),
+      UserId: user.get('id')
+    })
+  }
+}
+
+async function createFakeData() {
+  await createBars()
+  await createUsers()
+  await createMessages()
+}
+
+// models.sequelize.sync({force: true}).then(function() {
+//   createFakeData()
+// })
