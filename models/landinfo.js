bffa1f47a15624107ef12a554fbbf30b64e84697
larntin <larntin2002770@gmail.com>
2017-04-10 13:15:24 +0800

    添加用户登录酒吧接口landbar

---
+++ models/landinfo.js
@@ -0,0 +1,26 @@
+//order表需要添加User的外键id
+
+//由于有多种订单类型，添加tpye字段
+
+module.exports = (sequelize, DataTypes) => {
+  var LandInfo = sequelize.define('LandInfo', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userid: {
+      type: DataTypes.INTEGER
+    },
+    barid: {
+      type: DataTypes.INTEGER
+    }
+  }, {
+    //建表时是否包含所有timestamp字段，createdAt，updatedAt，deletedAt，这三个字段
+    timestamps: true,
+    //设置为true，在destroy时不会正真的删除而是，将deletedAt设置为删除的时间
+    paranoid: true
+  })
+
+  return LandInfo
+}
