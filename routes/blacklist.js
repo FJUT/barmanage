7b52b10a22d73b2ae413c194f35cb7bbe6c7d711
shao-jun <158020427@qq.com>
2017-04-27 17:09:02 +0800

    黑名单接口

--- routes/blacklist.js
+++ routes/blacklist.js
@@ -1,3 +1,39 @@
 /**
  * Created by shaojun on 2017/4/26.
  */
+// 黑名单操作
+router.post('/updateBlackList', (req, res, next) => {
+  co(function*() {
+    const barId = req.session.barInfo.id
+    const userId = req.body.userId
+    const isAdd = req.body.isAdd
+
+    if(isAdd) {
+      // 添加到黑名单
+      let black_list = models.BlackList.create({BarId: barId,UserId: userId})
+      if (black_list)
+        res.json({iRet: 0, msg: '插入记录成功'})
+      else
+        res.json({iRet: -1, msg: '插入BlackList失败'})
+    }
+    else{
+      // 移出黑名单
+      models.BlackList.destroy({
+        where: {
+          BarId: barId,
+          UserId: userId
+        }
+      }).then(deleteCount => {
+        res.json({
+          iRet: 0,
+          deleteCount: deleteCount
+        })
+      })
+    }
+
+  }).catch(err => {
+    res.json({
+      iRet: -1
+    })
+  })
+})
